b68e0497718d1ea18744554ec43c53c81debdad8
WalterJunior <walterjuniordossanto@gmail.com>
2022-02-01 14:50:16 -0300

    Funções

---
+++ script.js
@@ -0,0 +1,24 @@
+function Walter(){
+    console.log(Walter);
+}
+Walter();
+
+function itsme(eusou){
+    console.log(`hello it's me ${eusou}`);
+}
+itsme("Walter");
+
+function itsme(eusou,age,music){
+    console.log(`hello it's me ${eusou} i am ${age} years old and i love ${music} music`);
+}
+itsme("Walter","21","pop");
+
+function disney(filme,music){
+    console.log(`Tem um filme na Disney chamado ${filme} e tem uma musica linda que é "${music}"`);
+}
+disney("Moana","Saber quem sou");
+
+function multi (a,b){
+    return a * b;
+}
+console.log(multi (2,3));
